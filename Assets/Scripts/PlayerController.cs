a629c6e94896aa85706bbf6fc4f4a5602c326233
dennisbasa <201810646@gordoncollege.edu.ph>
2026-09-21 04:11:19 +0800

    refactor
    removed unused libraries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,5 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
